2fae1be00af712a64aa9636248d37026e6678ea8
mario_toledano <anotheranimal86@gmail.com>
2022-03-11 13:21:57 +0100

    Preparado para desplegar en tomcat

--- src/main/java/com/mariots/biblioteca/bibliotecaadmin/BibliotecaAdminApplication.java
+++ src/main/java/com/mariots/biblioteca/bibliotecaadmin/BibliotecaAdminApplication.java
@@ -2,17 +2,23 @@ package com.mariots.biblioteca.bibliotecaadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan({"model", "repository", "service", "controller"})
 @EnableJpaRepositories("repository")
 @SpringBootApplication
-public class BibliotecaAdminApplication {
+public class BibliotecaAdminApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaAdminApplication.class, args);
 		System.out.println("Inicio de la aplicación 2");
 	}
-
+	//Para arrancar con el war en apache tomcat not embebed
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BibliotecaAdminApplication.class);
+	}
 }
