ec176e84ba4c43cc3806bdf3a5c175d66c6d9de9
m.toledano <anotheranimal86@gmail.com>
2022-04-22 13:26:02 +0200

    actuator, arreglo endpoints ControllerBibliotecaRest añadidas notas estrategia de gestión de recursos HAL Explorer da conflicto con la dirección de index.html, separar web y rest en otro momento

--- src/main/java/com/mariots/biblioteca/bibliotecaadmin/core/controller/ControllerBibliotecaRest.java
+++ src/main/java/com/mariots/biblioteca/bibliotecaadmin/core/controller/ControllerBibliotecaRest.java
@@ -25,66 +25,66 @@ public class ControllerBibliotecaRest {
     @Autowired
     Mapper mapper;
 
-    //GET ALL
+    //GET resource -- Recuperar todos los resources de un tipo
     @GetMapping(value="/autores")
     public ResponseEntity recuperarAutores(){
         List<AutorDto> autores = service.recuperarAutores();
-        return new ResponseEntity<List<AutorDto>>(autores, HttpStatus.OK);
+        return new ResponseEntity<List<AutorDto>>(autores, HttpStatus.FOUND);
     }
     @GetMapping(value ="/textos")
     public ResponseEntity recuperarTextos(){
         List<TextoDto> textos= service.recupearTextos();
-        return new ResponseEntity<List<TextoDto>>(textos, HttpStatus.OK);
+        return new ResponseEntity<List<TextoDto>>(textos, HttpStatus.FOUND);
     }
     @GetMapping(value ="/temas")
     public ResponseEntity recuperarTemas(){
         List<TemaDto> temas= service.recuperarTemas();
-        return new ResponseEntity<List<TemaDto>>(temas, HttpStatus.OK);
+        return new ResponseEntity<List<TemaDto>>(temas, HttpStatus.FOUND);
     }
     @GetMapping(value ="/supertemas")
     public ResponseEntity recuperarSupertemas(){
         List<SupertemaDto> supertemas= service.recuperarSupertemas();
-        return new ResponseEntity<List<SupertemaDto>>(supertemas, HttpStatus.OK);
+        return new ResponseEntity<List<SupertemaDto>>(supertemas, HttpStatus.FOUND);
     }
 
-    //GET ID
+    //GET resource/{id} -- Recuperar recurso por id
     @GetMapping(value="/autores/{id}")
     public ResponseEntity recuperarAutoresPorId(@PathVariable int id){
         AutorDto autor = service.recuperarAutorPorId(id);
-        return new ResponseEntity<AutorDto>(autor, HttpStatus.OK);
+        return new ResponseEntity<AutorDto>(autor, HttpStatus.FOUND);
     }
     @GetMapping(value ="/textos/{id}")
     public ResponseEntity recuperarTextosPorId(@PathVariable int id){
         TextoDto texto = service.recuperarTextoPorId(id);
-        return new ResponseEntity<TextoDto>(texto, HttpStatus.OK);
+        return new ResponseEntity<TextoDto>(texto, HttpStatus.FOUND);
     }
     @GetMapping(value ="/temas/{id}")
     public ResponseEntity recuperarTemasPorId(@PathVariable int id){
         TemaDto tema = service.recuperarTemaPorId(id);
-        return new ResponseEntity<TemaDto>(tema, HttpStatus.OK);
+        return new ResponseEntity<TemaDto>(tema, HttpStatus.FOUND);
     }
     @GetMapping(value ="/supertemas/{id}")
     public ResponseEntity recuperarSupertemasPorId(@PathVariable int id){
         SupertemaDto supertema = service.recuperarSupertemaPorId(id);
-        return new ResponseEntity<SupertemaDto>(supertema, HttpStatus.OK);
+        return new ResponseEntity<SupertemaDto>(supertema, HttpStatus.FOUND);
     }
-    //GET NOMBRE
-    @GetMapping(value="/autores/{nombreAutor}")
+    //GET resource/nombre/{nombre} -- Recuperar recurso por nombre
+    @GetMapping(value="/autores/nombre/{nombreAutor}")
     public ResponseEntity recuperarAutoresPorNombre(@PathVariable String nombreAutor){
         AutorDto autor = service.recuperarAutorPorNombre(nombreAutor);
-        return new ResponseEntity<AutorDto>(autor, HttpStatus.OK);
+        return new ResponseEntity<AutorDto>(autor, HttpStatus.FOUND);
     }
-    @GetMapping(value ="/temas/{nombreTema}")
+    @GetMapping(value ="/temas/nombre/{nombreTema}")
     public ResponseEntity recuperarTemasPorNombre(@PathVariable String nombreTema){
         TemaDto tema = service.recuperarTemaPorNombre(nombreTema);
-        return new ResponseEntity<TemaDto>(tema, HttpStatus.OK);
+        return new ResponseEntity<TemaDto>(tema, HttpStatus.FOUND);
     }
-    @GetMapping(value ="/supertemas/{nombreSupertema}")
+    @GetMapping(value ="/supertemas/nombre/{nombreSupertema}")
     public ResponseEntity recuperarSupertemasPorId(@PathVariable String nombreSupertema) {
         SupertemaDto supertema = service.recuperarSupertemaPorNombre(nombreSupertema);
-        return new ResponseEntity<SupertemaDto>(supertema, HttpStatus.OK);
+        return new ResponseEntity<SupertemaDto>(supertema, HttpStatus.FOUND);
     }
-    //REGISTRAR NUEVO
+    //POST resource -- Creación nuevo resource
     @PostMapping(value="/autores")
     public ResponseEntity registrarNuevoAutor(@RequestBody NuevoAutorRest autor){
         AutorDto autorGuardado = service.guardarAutor(mapper.toDto(autor));
@@ -107,5 +107,12 @@ public class ControllerBibliotecaRest {
     public ResponseEntity registrarNuevoTexto(@RequestBody NuevoTextoRest texto){
         TextoDto textoGuardado = service.guardarTexto(mapper.toDto(texto));
         return new ResponseEntity<TextoDto>(textoGuardado, HttpStatus.CREATED);
+    }
+
+    //PUT vincular tema -->> supertema
+    @PutMapping(value="/temas/{idTema}/supertemas/{idSupertema}")
+    public ResponseEntity vincularTemaSupertema(@PathVariable int idTema, @PathVariable int idSupertema){
+        TemaDto temaGuardado= service.vincularTemaSupertema(idTema, idSupertema);
+        return new ResponseEntity<TemaDto>(temaGuardado, HttpStatus.CREATED);
     }
 }
